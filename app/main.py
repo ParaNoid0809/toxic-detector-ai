eca00780320a3f3e7db3903eada6d6694628372c
ParaNoid0809 <118473049+ParaNoid0809@users.noreply.github.com>
2025-04-06 17:33:24 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, HTTPException
 from app.model_text import analyze_text
 from app.model_image import analyze_image
 from app.schemas import TextInput, TextResult
+from fastapi.middleware.cors import CORSMiddleware
 import os
+import traceback
 
 port = int(os.environ.get("PORT", 8000))
 
@@ -10,7 +12,15 @@ if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=port)
 
+    
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],)
 
 @app.get("/")
 def read_root():
@@ -20,7 +30,12 @@ def read_root():
 async def analyze_text_route(payload: TextInput):
     return analyze_text(payload.text)
 
-@app.post("/analyze/image")
-async def analyze_image_route(file: UploadFile = File(...)):
-    contents = await file.read()
-    return analyze_image(contents)
+@app.post("/detect-nsfw-image")
+async def detect_nsfw_image(file: UploadFile = File(...)):
+    try:
+        contents = await file.read()
+        result = analyze_image(contents)
+        return result
+    except Exception as e:
+        traceback.print_exc()
+        raise HTTPException(status_code=500, detail=f"Error processing image: {str(e)}")
