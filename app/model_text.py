993e769aac4ac5b4283578d8ddc26d7dddb2591b
ParaNoid0809 <118473049+ParaNoid0809@users.noreply.github.com>
2025-04-06 06:01:34 +0530

    Update model_text.py

--- app/model_text.py
+++ app/model_text.py
@@ -1,18 +1,6 @@
 from transformers import pipeline
+from functools import lru_cache
 
-# Load model
-classifier = pipeline("text-classification", model="unitary/toxic-bert")
-
-def analyze_text(text: str):
-    result = classifier(text)[0]
-    toxicity = round(result["score"], 3)
-    label = result["label"].lower()
-
-    flags = []
-    if label == "toxic":
-        flags.append("toxic")
-
-    return {
-        "toxicity": toxicity,
-        "flags": flags
-    }
+@lru_cache(maxsize=1)
+def get_text_toxic_classifier():
+    return pipeline("text-classification", model="unitary/toxic-bert", top_k=None)
